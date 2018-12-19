3d7106063acd10cf57ec74bb87ef958c1e264616
Alex <>
2018-12-20 02:23:03 +0800

    修改ALTableKit.h

--- ALTableKit/ALTableKit.h
+++ ALTableKit/ALTableKit.h
@@ -6,14 +6,11 @@
 //  Copyright © 2018 Alex. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for ALTableKit.
-FOUNDATION_EXPORT double ALTableKitVersionNumber;
-
-//! Project version string for ALTableKit.
-FOUNDATION_EXPORT const unsigned char ALTableKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ALTableKit/PublicHeader.h>
-
-
+#import "ALTableContext.h"
+#import "ALTableAdapter.h"
+#import "ALTableDiffable.h"
+#import "ALTableSectionController.h"
+#import "ALTableDelegate.h"
+#import "ALTableDataSource.h"
+#import "ALTableDisplayDelegate.h"
+#import "ALTableScrollDelegate.h"

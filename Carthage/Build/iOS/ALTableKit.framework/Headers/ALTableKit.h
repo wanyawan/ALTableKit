61431cd404d5fc546527a338ca791733cf3c7dd2
Alex <>
2019-02-02 15:50:34 +0800

    move ALTableKit.h public group

---
+++ Carthage/Build/iOS/ALTableKit.framework/Headers/ALTableKit.h
@@ -0,0 +1,19 @@
+//
+//  ALTableKit.h
+//  ALTableKit
+//
+//  Created by Alex on 2018/12/19.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+#import <ALTableKit/ALTableContext.h>
+#import <ALTableKit/ALTableAdapter.h>
+#import <ALTableKit/ALTableDiffable.h>
+#import <ALTableKit/ALTableHeaderFooterViewSource.h>
+#import <ALTableKit/ALTableSectionController.h>
+#import <ALTableKit/ALTableDelegate.h>
+#import <ALTableKit/ALTableDataSource.h>
+#import <ALTableKit/ALTableDisplayDelegate.h>
+#import <ALTableKit/ALTableScrollDelegate.h>
+#import <ALTableKit/ALTableSectionProvider.h>
+#import <ALTableKit/ALTableComplexSectionController.h>
